fee30594d1e356036b66b4e9d11a6970c501a1a0
Aaron Parecki <aaron@parecki.com>
2018-11-06 12:35:45 -0800

    add working app!

---
+++ public/js/script.js
@@ -0,0 +1,63 @@
+window.addEventListener('online',  updateOnlineStatus);
+window.addEventListener('offline', updateOnlineStatus);
+
+function updateOnlineStatus(event) {
+  console.log("Device is "+(navigator.onLine ? "online" : "offline"));
+
+  if(navigator.onLine) {
+    $("body").addClass("online").removeClass("offline");
+    if(window.syncAllPosts) {
+      console.log('syncing posts');
+      refreshSavedPosts();
+      syncAllPosts();
+    }
+  } else {
+    $("body").addClass("offline").removeClass("online");
+    if(window.refreshSavedPosts) {
+      console.log('loading posts');
+      refreshSavedPosts();
+    }
+  }
+}
+
+function tz_seconds_to_offset(seconds) {
+  var hours = zero_pad(Math.floor(Math.abs(seconds / 60 / 60)));
+  var minutes = zero_pad(Math.floor(Math.abs(seconds) / 60) % 60);
+  return (seconds < 0 ? '-' : '+') + hours + ":" + minutes;
+}
+
+function tz_minutes_to_offset(minutes) {
+  var hours = zero_pad(Math.floor(Math.abs(minutes / 60)));
+  var min = zero_pad(Math.abs(minutes) % 60);
+  return (minutes < 0 ? '-' : '+') + hours + ":" + min;
+}
+
+function tz_offset_to_minutes(offset) {
+  var match = offset.match(/([+-])(\d\d):?(\d\d)/);
+  var minutes = parseInt(match[3]);
+  minutes = minutes + (parseInt(match[2] * 60));
+  if(match[1] == '-') {
+    minutes = minutes * -1;
+  }
+  return minutes;
+}
+
+function zero_pad(num) {
+  num = "" + num;
+  if(num.length == 1) {
+    num = "0" + num;
+  }
+  return num;
+}
+
+function dateToYMD(d) {
+  return d.getFullYear()+"-"+zero_pad(d.getMonth()+1)+"-"+zero_pad(d.getDate());
+}
+
+function dateToUTCYMD(d) {
+  return d.getUTCFullYear()+"-"+zero_pad(d.getUTCMonth()+1)+"-"+zero_pad(d.getUTCDate());
+}
+
+function dateToUTCHM(d) {
+  return d.getUTCHours()+":"+zero_pad(d.getUTCMinutes());
+}
---
+++ public/serviceworker.js
@@ -0,0 +1,103 @@
+"use strict";
+
+const version = 7;
+const staticCacheName = 'airplanetime-'+version;
+
+// Cache all the required assets when the ServiceWorker installs
+addEventListener('install', function (event) {
+  console.log('The service worker is installing...');
+  skipWaiting();
+  event.waitUntil(
+    cacheAssets()
+  ); // end waitUntil
+});
+
+// When the ServiceWorker is activated, update the cache if the cache name changed
+addEventListener('activate', activateEvent => {
+  console.log('The service worker is activated.');
+
+  activateEvent.waitUntil(
+    caches.keys()
+    .then( cacheNames => {
+      return Promise.all(
+        cacheNames.map( cacheName => {
+          if (cacheName != staticCacheName) {
+            return caches.delete(cacheName);
+          } // end if
+        }) // end map
+      ); // end return Promise.all
+    }) // end keys then
+    .then( () => {
+      var claim = clients.claim();
+      sendAlert('version-'+version);
+      return claim;
+    }) // end then
+  );
+
+});
+
+addEventListener('message', messageEvent => {
+  console.log('Got message', messageEvent);
+
+  if(messageEvent.data == 'version') {
+    sendAlert('version-'+version);
+    return;
+  }
+
+});
+
+// Intercept HTTP requests
+addEventListener("fetch", fetchEvent => {
+  var t = fetchEvent.request, a = new URL(t.url);
+
+  const request = fetchEvent.request;
+  fetchEvent.respondWith(
+
+    // First look in the cache
+    caches.match(request)
+    .then(responseFromCache => {
+      if(responseFromCache) {
+        return responseFromCache;
+      }
+
+      // Otherwise fetch from the network
+      return fetch(request);
+
+    }) // end match then
+
+  ); // end respondWith
+}); // end event listener
+
+function cacheAssets() {
+  caches.open(staticCacheName)
+  .then( cache => {
+    // Nice to have, won't error if these fail
+    cache.addAll([
+      'images/airplane-16px.png',
+      'images/airplane-icon-57.png',
+      'images/airplane-icon-72.png',
+      'images/airplane-icon-114.png',
+      'images/airplane-icon-144.png',
+    ]);
+
+    // Must have, will error if they fail to download
+    return cache.addAll([
+      '/',
+      '/settings.html',
+      '/style.css',
+      '/js/jquery-3.3.1.js',
+      '/js/script.js',
+      '/bootstrap-4.1.3/css/bootstrap.min.css',
+      '/bootstrap-4.1.3/js/bootstrap.min.js',
+    ]);
+  })
+}
+
+
+function sendAlert(alert) {
+  clients.matchAll().then(clients => {
+    clients.forEach(client => {
+      client.postMessage(alert);
+    })
+  })
+}
